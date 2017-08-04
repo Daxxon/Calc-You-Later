12fc2002495ca8b32364cf0e4af0c036720c112d
Daniel Waggoner <daxxon37@hotmail.com>
2017-08-04 11:36:53 -0400

    added id's for each button

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let buttonLabelsArray = ['C', 'Hello, I am Calculon', '7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+'];
+let buttonIdArray = ['clear', 'output', 'seven', 'eight', 'nine', 'divi', 'four', 'five', 'six', 'mult', 'one', 'two', 'three', 'sub', 'zero', 'dot', 'equals', 'add'];
 myCalculator = document.getElementById('myCalculator');
 for (let i=0; i<buttonLabelsArray.length; i++) {
   let newBtn = document.createElement('div');
@@ -8,8 +9,6 @@ for (let i=0; i<buttonLabelsArray.length; i++) {
   newBtn.appendChild(btnLabelBox);
   btnLabelBox.appendChild(btnLabel);
   newBtn.className = 'calculatorButton';
+  newBtn.id = buttonIdArray[i];
   myCalculator.appendChild(newBtn);
-  if (i === 1) {
-    newBtn.id = 'output';
-  }
 }
