e544c5302b802fde8386f2db19ecd4eed57e8156
Daniel Waggoner <daxxon37@hotmail.com>
2017-08-09 19:17:03 -0400

    final submission, succumbed to eval

--- main.js
+++ main.js
@@ -1,14 +1,44 @@
-let buttonLabelsArray = ['C', 'Hello, I am Calculon', '7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+'];
-let buttonIdArray = ['clear', 'output', 'seven', 'eight', 'nine', 'divi', 'four', 'five', 'six', 'mult', 'one', 'two', 'three', 'sub', 'zero', 'dot', 'equals', 'add'];
-myCalculator = document.getElementById('myCalculator');
-for (let i=0; i<buttonLabelsArray.length; i++) {
-  let newBtn = document.createElement('div');
-  let btnLabelBox = document.createElement('p');
-  let btnLabel = document.createTextNode(buttonLabelsArray[i]);
-  btnLabelBox.className = 'btnLabelBox';
-  newBtn.appendChild(btnLabelBox);
-  btnLabelBox.appendChild(btnLabel);
-  newBtn.className = 'calculatorButton';
-  newBtn.id = buttonIdArray[i];
-  myCalculator.appendChild(newBtn);
+// popYouLater();
+//
+// function popYouLater() {
+//   let buttonLabelsArray = ['C', 'Hello, I am Calculon', '7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+'];
+//   let buttonIdArray = ['clear', 'output', 'seven', 'eight', 'nine', 'divi', 'four', 'five', 'six', 'mult', 'one', 'two', 'three', 'sub', 'zero', 'dot', 'equals', 'add'];
+//   myCalculator = document.getElementById('myCalculator');
+//   for (let i=0; i<buttonLabelsArray.length; i++) {
+//     let newBtn = document.createElement('button');
+//     let btnLabelBox = document.createElement('p');
+//     let btnLabel = document.createTextNode(buttonLabelsArray[i]);
+//     btnLabelBox.className = 'btnLabelBox';
+//     newBtn.appendChild(btnLabelBox);
+//     btnLabelBox.appendChild(btnLabel);
+//     newBtn.className = 'calculatorButton';
+//     newBtn.id = buttonIdArray[i];
+//     myCalculator.appendChild(newBtn);
+//   }
+// }
+myLCD = document.getElementById('output');
+myLCD.innerHTML = '';
+let message = "Hello, I am Calculon";
+let tempStorage = '';
+
+function appendNum(myInput) {
+  myLCD.innerHTML += myInput;
+}
+
+function appendOperator(myInput) {
+  myLCD.innerHTML += myInput;
+}
+
+function calculate() {
+  if (eval(myLCD.innerHTML) !== undefined) {
+    myLCD.innerHTML = eval(myLCD.innerHTML);
+  }
+}
+
+function secretMessage() {
+  alert("Please do not click the LCD panel.")
+}
+
+function clearLCD() {
+  myLCD.innerHTML = '';
 }
